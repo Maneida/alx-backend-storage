609b151f1994a29963fea1bc272a93deb6df56b2
Michael Amuah <mkdamuah@hotmail.com>
2023-10-13 00:10:57 +0000

    corr 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Assuming metal_bands table is already imported with columns 'origin' and 'nb_fans'
 
-SELECT origin, SUM(nb_fans) as total_fans
+SELECT origin, SUM(nb_fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY total_fans DESC;
+ORDER BY nb_fans DESC;
