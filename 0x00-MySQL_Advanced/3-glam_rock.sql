9b332e599cca39daae329647b9dfcff6b75674f5
Michael Amuah <mkdamuah@hotmail.com>
2023-10-13 00:26:28 +0000

    corr 2-4

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,15 +1,11 @@
--- Create a temporary table to store split dates
-CREATE TEMPORARY TABLE temp_dates AS 
-SELECT 
-  band_name,
-  STR_TO_DATE(SUBSTRING_INDEX(SUBSTRING_INDEX(split, ' - ', 1), ' ', 1), '%Y') as start_year,
-  STR_TO_DATE(SUBSTRING_INDEX(SUBSTRING_INDEX(split, ' - ', -1), ' ', -1), '%Y') as end_year
-FROM metal_bands;
+-- SQL script that lists all bands with Glam rock as
+-- their main style, ranked by their longevity
 
--- Calculate lifespan in years
-SELECT 
-  band_name,
-  YEAR('2022-01-01') - YEAR(start_year) as lifespan
-FROM temp_dates
-WHERE start_year IS NOT NULL AND end_year IS NOT NULL
+SELECT band_name,
+         CASE
+         WHEN split IS NULL THEN (2022 - formed)
+         ELSE (split - formed)
+         END AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
